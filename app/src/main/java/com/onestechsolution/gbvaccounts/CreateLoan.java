ce221190a4c941ccd5350d2c1acb35b79d4021cf
Ganesh Achary <PSGanesh@Ganeshs-MacBook-Pro.local>
2017-04-13 20:36:17 +0530

    Initial commit

---
+++ app/src/main/java/com/onestechsolution/gbvaccounts/CreateLoan.java
@@ -0,0 +1,13 @@
+package com.onestechsolution.gbvaccounts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateLoan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_loan);
+    }
+}
