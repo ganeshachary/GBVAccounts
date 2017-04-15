a62901f63d530aa51ea1459b1d9e556010501b67
ganesh <acharya.ganesh2@gmail.com>
2017-04-16 02:03:40 +0530

    added karigar form in the application

---
+++ app/src/main/java/com/onestechsolution/gbvaccounts/karigar.java
@@ -0,0 +1,13 @@
+package com.onestechsolution.gbvaccounts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class karigar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_karigar);
+    }
+}
